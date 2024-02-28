3656754bfa4c71d6636bb27ab10ebe27df874311
JaywenTan <160487275+JaywenTan@users.noreply.github.com>
2024-02-28 01:55:38 +0000

    Final Submission

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ seconds = int(seconds)
 
 # ... complete the code below
 
+seconds = divmod(seconds,3600)
+hours = seconds[0]
+seconds = divmod(seconds[1],60)
+minutes = seconds[0]
+seconds = seconds[1]
 
 
 # Follow the formatting given
